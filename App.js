8cbe1188f0d378897dc66973a700c5e4aa581391
sarah-koehler <s.koehler@searchmetrics.com>
2018-05-25 10:29:31 +0200

    use react-native-dotenv for api key in dev

--- App.js
+++ App.js
@@ -6,6 +6,8 @@ import { setContext } from 'apollo-link-context';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { REACT_APP_API_KEY, REACT_APP_API_SECRET } from 'react-native-dotenv';
+
 import HelloScreen from './screens/HelloScreen';
 import HomeScreen from './screens/HomeScreen';
 import { createStackNavigator } from 'react-navigation';
@@ -18,8 +20,8 @@ const httpLink = new HttpLink({
 const authLink = setContext((_, { headers }) => ({
   headers: {
     ...headers,
-    "sm-api-key": process.env.REACT_APP_API_KEY,
-    "sm-api-secret": process.env.REACT_APP_API_SECRET,
+    "sm-api-key": REACT_APP_API_KEY,
+    "sm-api-secret": REACT_APP_API_SECRET,
   }
 }))
 
